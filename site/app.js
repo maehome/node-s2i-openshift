376630ec7f71a2be1c6062a1cda75cb5aacecb49
maehome <maehome@yahoo.com>
2020-05-07 17:13:44 -0400

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,11 +52,11 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
+        "name": "John Doe",
+        "age": 40,
         "gender": "male",
-        "street": "34 Main Street",
-        "city": "Toronto",
+        "street": "34 Happy Street",
+        "city": "Streetsville",
         "zipcode": "M5H 1T1"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
